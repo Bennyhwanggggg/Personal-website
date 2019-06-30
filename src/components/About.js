071c160b5a19165faa2ba1b0091aadcce4b69f2c
Mitsunari <bennyhwanggggg@users.noreply.github.com>
2019-06-30 22:18:39 +1000

    update contact in about

--- src/components/About.js
+++ src/components/About.js
@@ -27,8 +27,8 @@ export default class About extends Component {
 
                   <h2>Contact Details</h2>
                   <p className="address">
-       						<span>{resumeData.name}</span>
-                     <br></br>
+       						{/* <span>{resumeData.name}</span>
+                     <br></br> */}
        						   <span>
                      {resumeData.address}
                     </span>
