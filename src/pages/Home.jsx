9bb74191815acdd07b12d1cbcf749e0033555ffe
Benny Hwang <32606136+Bennyhwanggggg@users.noreply.github.com>
2024-04-20 18:07:59 +1000

    add intro text and move image down

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -9,26 +9,51 @@ export const Home = () => {
         <>
             <NavBar/>
             <Grid container
-                spacing={1}
+                spacing={0}
                 display={'flex'}
                 justifyContent={'center'}
                 alignItems={'center'}
-                paddingTop={'6vh'}>
+                paddingTop={'10vh'}>
                 <Grid item xs={12} sm={6} display={'flex'} sx={{justifyContent: { xs: 'center', sm: 'flex-end'}}}>
                     <Paper elevation={3} sx={{
                         backgroundColor: `${theme.palette.primary.main}`,
-                        width: '60%',
+                        width: '50%',
                     }}>
                         <Container sx={{ padding: '1em'}}>
-                            <Typography variant='h4' sx={{color: `${theme.palette.text.primary}`}}
-                                >Hi I'm Benny
+                            <Typography paragraph
+                                fontWeight={'bold'}
+                                variant='h5'
+                                sx={{color: `${theme.palette.text.primary}`}}
+                                >Hi I'm Benny!
+                            </Typography>
+                            <Typography fontSize={16}
+                                variant='body1'
+                                paragraph
+                                lineHeight={2}
+                                sx={{color: `${theme.palette.text.primary}`}}
+                                >I am a software engineer based in Sydney, Australia. I am currently a senior software engineer at Atlassian working on AI platform.
+                                Previously, I worked at Google, Amazon (AWS), startups and other Australian big techs.
+                            </Typography>
+                            <Typography fontSize={16}
+                                variant='body1'
+                                paragraph
+                                lineHeight={2}
+                                sx={{color: `${theme.palette.text.primary}`}}
+                                >My skills range from building mobile apps, web apps to large scale distributed systems.
+                            </Typography>
+                            <Typography fontSize={16}
+                                variant='body1'
+                                paragraph
+                                lineHeight={2}
+                                sx={{color: `${theme.palette.text.primary}`}}
+                                >👋 Contact me on: bennyhwangg@gmail.com
                             </Typography>
                         </Container>
                     </Paper>
                 </Grid>
                 <Grid item xs={12} sm={6} display={'flex'} sx={{justifyContent: { xs: 'center', sm: 'flex-start'}}}>
                     <Box component='img'
-                        sx={{ width: '60%'}}
+                        sx={{ width: '40%', minWidth: '300px', marginTop: { sm: '15vh'}}}
                         alt='Profile image'
                         src={`${process.env.PUBLIC_URL}/profile_image.jpg`}
                         borderRadius={4}
