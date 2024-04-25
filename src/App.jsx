d71494f46ec5f5aea35d8afbffc2fe95030b188e
Benny Hwang <32606136+Bennyhwanggggg@users.noreply.github.com>
2024-04-25 13:38:56 +1000

    Add router dom and fix nav link

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Home } from './pages/Home';
 import { ThemeProvider } from '@mui/material/styles';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import theme from './theme/theme';
 
 const App = () => {
   return (
     <ThemeProvider theme={theme}>
-      <div className="App">
-        <Home></Home>
-      </div>
+      <Router>
+        <div className="App">
+          <Routes>
+            <Route path='/' element={<Home />} />
+            <Route path='/home' element={<Home />} />
+          </Routes>
+        </div>
+      </Router>
     </ThemeProvider>
   );
 };
