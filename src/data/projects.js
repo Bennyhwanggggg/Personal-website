8316970617e1b5e6566aa8e2c4c473f5d9723f6a
Benny Hwang <32606136+Bennyhwanggggg@users.noreply.github.com>
2024-04-27 23:35:40 +1000

    Set up project section

---
+++ src/data/projects.js
@@ -0,0 +1,22 @@
+export const projects = [
+    {
+        title: 'Super Mario Bro Clone',
+        url: 'http://super-mario-clone-js.ap-northeast-1.elasticbeanstalk.com/',
+        description: 'A rough prototype version of Super Mario Bro on your web browser!',
+    },
+    {
+        title: 'Grow what you eat',
+        url: 'https://fogo.netlify.app/',
+        description: 'Transform your meals into environmental action! Upload food photos to witness immediate changes in our virtual world.',
+    },
+    {
+        title: 'Patatap',
+        url: 'https://bennyhwanggggg.github.io/Simple-Patatap/',
+        description: 'Tap, create, and explore a world of sound and color with every keystroke.',
+    },
+    {
+        title: 'Sprout Design 萌芽設計',
+        url: 'https://bennyhwanggggg.github.io/Sprout-Design-Company-Web-Page/',
+        description: 'Contract work for a Taiwan based graphics design company.',
+    },
+];
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,69 +1,111 @@
 import React from 'react';
 import NavBar from '../components/NavBar';
-import { Grid, Paper, Typography, Container, Box } from '@mui/material';
+import { Grid, Paper, Typography, Container, Box, CardHeader } from '@mui/material';
 import { useTheme } from '@mui/material';
+import Card from '@mui/material/Card';
+import CardContent from '@mui/material/CardContent';
+import { projects } from '../data/projects';
 
 export const Home = () => {
     const theme = useTheme();
     return (
         <>
             <NavBar/>
-            <Grid container
-                backgroundColor={'white'}
-                spacing={0}
-                display={'flex'}
-                justifyContent={'center'}
-                alignItems={'center'}
+            <Box backgroundColor={'white'}
                 paddingTop={'10vh'}
                 paddingBottom={'10vh'}>
-                <Grid item xs={12} sm={6} display={'flex'} sx={{justifyContent: { xs: 'center', sm: 'flex-end'}}}>
-                    <Paper elevation={3} sx={{
-                        backgroundColor: `${theme.palette.primary.main}`,
-                        width: '50%',
-                        minWidth: '300px',
-                        marginBottom: { sm: '15vh'},
-                    }}>
-                        <Container sx={{ padding: '1em'}}>
-                            <Typography paragraph
-                                fontWeight={'bold'}
-                                variant='h5'
-                                sx={{color: `${theme.palette.text.primary}`}}
-                                >Hi I'm Benny!
-                            </Typography>
-                            <Typography fontSize={16}
-                                variant='body1'
-                                paragraph
-                                lineHeight={2}
-                                sx={{color: `${theme.palette.text.primary}`}}
-                                >I am a software engineer based in Sydney, Australia. I am currently a senior software engineer at Atlassian working on AI platform.
-                                Previously, I worked at Google, Amazon (AWS), startups and other Australian big techs.
-                            </Typography>
-                            <Typography fontSize={16}
-                                variant='body1'
-                                paragraph
-                                lineHeight={2}
-                                sx={{color: `${theme.palette.text.primary}`}}
-                                >My skills range from building mobile apps, web apps to large scale distributed systems.
-                            </Typography>
-                            <Typography fontSize={16}
-                                variant='body1'
-                                paragraph
-                                lineHeight={2}
-                                sx={{color: `${theme.palette.text.primary}`}}
-                                >👋 Get in Touch: bennyhwangg@gmail.com
-                            </Typography>
-                        </Container>
-                    </Paper>
+                <Grid container
+                    backgroundColor={'white'}
+                    spacing={0}
+                    display={'flex'}
+                    justifyContent={'center'}
+                    alignItems={'center'}
+                >
+                    <Grid item xs={12} sm={6} display={'flex'} sx={{justifyContent: { xs: 'center', sm: 'flex-end'}}}>
+                        <Paper elevation={3} sx={{
+                            backgroundColor: `${theme.palette.primary.main}`,
+                            width: '50%',
+                            minWidth: '300px',
+                            marginBottom: { sm: '15vh'},
+                        }}>
+                            <Container sx={{ padding: '1em'}}>
+                                <Typography paragraph
+                                    fontWeight={'bold'}
+                                    variant='h5'
+                                    sx={{color: `${theme.palette.text.primary}`}}
+                                    >Hi I'm Benny!
+                                </Typography>
+                                <Typography fontSize={16}
+                                    variant='body1'
+                                    paragraph
+                                    lineHeight={2}
+                                    sx={{color: `${theme.palette.text.primary}`}}
+                                    >I am a software engineer based in Sydney, Australia. I am currently a senior software engineer at Atlassian working on AI platform.
+                                    Previously, I worked at Google, Amazon (AWS), startups and other Australian big techs.
+                                </Typography>
+                                <Typography fontSize={16}
+                                    variant='body1'
+                                    paragraph
+                                    lineHeight={2}
+                                    sx={{color: `${theme.palette.text.primary}`}}
+                                    >My skills range from building mobile apps, web apps to large scale distributed systems.
+                                </Typography>
+                                <Typography fontSize={16}
+                                    variant='body1'
+                                    paragraph
+                                    lineHeight={2}
+                                    sx={{color: `${theme.palette.text.primary}`}}
+                                    >👋 Get in Touch: bennyhwangg@gmail.com
+                                </Typography>
+                            </Container>
+                        </Paper>
+                    </Grid>
+                    <Grid item xs={12} sm={6} display={'flex'} sx={{justifyContent: { xs: 'center', sm: 'flex-start'}}}>
+                        <Box component='img'
+                            sx={{ width: '40%', minWidth: '300px', marginTop: { sm: '15vh'}}}
+                            alt='Profile image'
+                            src={`${process.env.PUBLIC_URL}/profile_image.jpg`}
+                            borderRadius={4}
+                            />
+                    </Grid>
                 </Grid>
-                <Grid item xs={12} sm={6} display={'flex'} sx={{justifyContent: { xs: 'center', sm: 'flex-start'}}}>
-                    <Box component='img'
-                        sx={{ width: '40%', minWidth: '300px', marginTop: { sm: '15vh'}}}
-                        alt='Profile image'
-                        src={`${process.env.PUBLIC_URL}/profile_image.jpg`}
-                        borderRadius={4}
-                         />
-                </Grid>
-            </Grid>
+                <Box marginLeft={'10vw'}
+                    marginRight={'10vw'}
+                    marginTop={'5vh'}
+                    marginBottom={'5vh'}>
+                    <Typography paragraph
+                        fontWeight={'bold'}
+                        variant='h5'
+                        sx={{color: `${theme.palette.text.primary}`}}
+                        >Things I've built
+                    </Typography>
+                    <Grid container
+                        spacing={2}
+                        display={'flex'}
+                        justifyContent={'flex-start'}
+                        alignItems={'center'}
+                    >
+                        {projects.map((project, index) => {
+                            return (
+                                <Grid key={index}
+                                    item
+                                    xs={12} sm={4}
+                                    minWidth={250}
+                                    display={'flex'}>
+                                    <Card sx={{ width: '100%',
+                                        height: '30vh'}}
+                                        elevation={3}>
+                                        <CardHeader title={project.title}/>
+                                        <CardContent>
+                                            <Typography>{project.description}</Typography>
+                                        </CardContent>
+                                    </Card>
+                                </Grid>
+                            );
+                        })}
+                    </Grid>
+                </Box>
+            </Box>
         </>
     );
 };
